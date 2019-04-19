8c9be56065bbc1a1977ba70a5ae0c601e16b7256
Eoghan <eoghan.mc-dermott.1@ucdconnect.ie>
2019-04-19 15:33:19 +0100

    Forward pass added & changed network architecture

--- src/MLP.java
+++ src/MLP.java
@@ -1,3 +1,4 @@
+import java.util.Arrays;
 import java.util.Random;
 
 public class MLP
@@ -5,25 +6,30 @@ public class MLP
     private int numInputs;
     private int numHiddenUnits;
     private int numOutputs;
-    private double[][] w1, w2;
-    private double[][] dW1, dW2;
-    private double[][] z1, z2;
+
+    private double[] w1, w2;
+    private double[] dW1, dW2;
+
+    private double[] z1, z2;
+
     private double[] hidden;
+
     private double[] outputs;
 
 
 
     public MLP(int ni, int nh, int no)
     {
+
         numInputs = ni;
         numHiddenUnits = nh;
         numOutputs = no;
 
-        w1 = new double[numInputs][numHiddenUnits];
-        w2 = new double[numOutputs][numHiddenUnits];
+        w1 = new double [numInputs];
+        w2 = new double [numHiddenUnits];
 
-        z1 = new double[numHiddenUnits][1];
-        z2 = new double[numHiddenUnits][1];
+        z1 = new double[numHiddenUnits];
+        z2 = new double[numHiddenUnits];
 
         hidden = new double[numHiddenUnits];
         outputs = new double[numOutputs];
@@ -31,16 +37,18 @@ public class MLP
 
         randomise(w1);
         randomise(w2);
+        Arrays.fill(outputs, 0);
         //fill weight arrays with small random initial weights
     }
 
-    private void randomise(double[][] input)//method to fill 2d array with small random values
+    private void randomise(double[] input)//method to fill 2d array with small random values
     {
-        for(double[] w: input)
-            for(double weight : w) {
-                weight = generateInitialWeight();
-                System.out.println(weight);
-            }
+
+        for(double weight : input)
+        {
+            weight = generateInitialWeight();
+            System.out.println(weight);
+        }
 
     }
 
@@ -58,6 +66,30 @@ public class MLP
         return initialWeight;
     }
 
+    public void forwardPass(double[] input)
+    {
+        if(input.length != numInputs)
+            System.out.println("Invalid input");
+        //check to make sure dimensions are okay
+
+        hidden = multiply(input,w1);
+
+        for(double h : hidden)
+            h = sigmoid(h);
+        //multiply inputs by weights in lower layer then apply activation function
+
+        outputs = multiply(hidden, w2);
+
+        for(double o : outputs)
+            o = sigmoid(o);
+        //multiply hidden values by weights in upper layer and apply activation function
+    }
+
+    public void backProp(double input)
+    {
+
+    }
+
     private double sigmoid(double x)
     {
         return (1.0/1.0 + Math.exp(-x));
@@ -66,6 +98,21 @@ public class MLP
     private double sigmoidDerivative(double x)
     {
         return sigmoid(x)*(1.0-sigmoid(x));
+    }
+
+    private double[] multiply(double[] a1, double[] a2)
+    {
+        double[] output = new double[a1.length];
+
+        for(double  value: output)
+        {
+            value = 0;
+
+            for (int i = 0; i < a1.length; i++)
+                value += a1[i] * a2[i];
+        }
+
+        return output;
     }
 
 }
