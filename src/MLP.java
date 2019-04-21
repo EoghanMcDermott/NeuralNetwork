1d92cc97b715009dffa59b5eb8344ce5b30c2c33
Eoghan <eoghan.mc-dermott.1@ucdconnect.ie>
2019-04-21 16:20:18 +0100

    Reverted to original network structure and implemented forward propagation

--- src/MLP.java
+++ src/MLP.java
@@ -7,8 +7,8 @@ public class MLP
     private int numHiddenUnits;
     private int numOutputs;
 
-    private double[] w1, w2;
-    private double[] dW1, dW2;
+    private double[][] w1, w2;
+    private double[][] dW1, dW2;
 
     private double[] z1, z2;
 
@@ -25,8 +25,8 @@ public class MLP
         numHiddenUnits = nh;
         numOutputs = no;
 
-        w1 = new double [numInputs];
-        w2 = new double [numHiddenUnits];
+        w1 = new double [numInputs][numHiddenUnits];
+        w2 = new double [numHiddenUnits][numOutputs];
 
         z1 = new double[numHiddenUnits];
         z2 = new double[numHiddenUnits];
@@ -35,84 +35,111 @@ public class MLP
         outputs = new double[numOutputs];
         //initialise various arrays
 
-        randomise(w1);
-        randomise(w2);
+        w1 = randomise(w1);
+        w2 = randomise(w2);
+        Arrays.fill(hidden,0);
         Arrays.fill(outputs, 0);
         //fill weight arrays with small random initial weights
     }
 
-    private void randomise(double[] input)//method to fill 2d array with small random values
+    private double[][] randomise(double[][] input)//method to fill 2d array with small random values
     {
+        double[][] result = new double[input.length][input[0].length];
 
-        for(double weight : input)
+        for(int i=0;i<input.length;i++)
         {
-            weight = generateInitialWeight();
-            System.out.println(weight);
+            for(int j=0;j<input[0].length;j++)
+                result[i][j] = generateInitialWeight();
         }
 
+        return result;
     }
 
     private double generateInitialWeight()//method to generate small initial weights
     {
         Random rand = new Random();
-
-        int numerator = rand.nextInt(10);
-
-        double initialWeight = numerator/1000.0;
-
-        if(initialWeight == 0)
-            initialWeight += 0.05;
-
-        return initialWeight;
+//
+//        int numerator = rand.nextInt(10);
+//
+//        double initialWeight = numerator/1000.0;
+//
+//        if(initialWeight == 0)
+//            initialWeight += 0.05;
+//
+//        return initialWeight;
+        return rand.nextDouble();
     }
 
     public void forwardPass(double[] input)
     {
-        if(input.length != numInputs)
-            System.out.println("Invalid input");
-        //check to make sure dimensions are okay
-
-        hidden = multiply(input,w1);
-
-        for(double h : hidden)
-            h = sigmoid(h);
-        //multiply inputs by weights in lower layer then apply activation function
-
-        outputs = multiply(hidden, w2);
-
-        for(double o : outputs)
-            o = sigmoid(o);
-        //multiply hidden values by weights in upper layer and apply activation function
+        calculateHidden(input);
+        calculateOutput();
     }
 
-    public void backProp(double input)
+    public void backProp(double target)
     {
 
     }
 
-    private double sigmoid(double x)
+    public void updateWeights(double learningRate)
+    {
+        //W1 += learningRate*dW1;
+       // W2 += learningRate*dW2;
+
+        Arrays.fill(dW1,0);
+        Arrays.fill(dW2,0);
+    }
+
+    private double sigmoid(double x)//activation function
     {
         return (1.0/1.0 + Math.exp(-x));
     }
 
-    private double sigmoidDerivative(double x)
+    private double sigmoidDerivative(double x)//activation function derivative for backpropagation
     {
         return sigmoid(x)*(1.0-sigmoid(x));
     }
 
-    private double[] multiply(double[] a1, double[] a2)
+    private void calculateHidden(double[] input)
     {
-        double[] output = new double[a1.length];
+        System.out.println("w1 values:");
+        for(double[] row : w1)
+            for(double w : row)
+                System.out.println(w);
 
-        for(double  value: output)
+        //using matrix multiplication to calculate values in hidden units
+        for(int i=0;i<numHiddenUnits;i++)
         {
-            value = 0;
-
-            for (int i = 0; i < a1.length; i++)
-                value += a1[i] * a2[i];
+            for(int j=0;j<numInputs;j++)
+                hidden[i] += input[j] * w1[j][i];
         }
 
-        return output;
+        System.out.println("Hidden before activation:");
+        for(double h : hidden)
+            System.out.println("Hidden: " + h);
+
+        for(int i=0;i<numHiddenUnits;i++)
+        {
+            hidden[i] = sigmoid(hidden[i]);//applying activation function
+            z1[i] = hidden[i];
+        }
     }
 
+    private void calculateOutput()
+    {
+       //w2 = new double[][]{{0.3},{0.5}};
+
+        for(int i=0;i<numOutputs;i++)
+        {
+            for(int j=0;j<numInputs;j++)
+                outputs[i] += hidden[j] * w2[j][i];
+        }
+
+        System.out.println("Output: " + outputs[0]);
+        for(double o: outputs)//applying activation function
+            o = sigmoid(o);
+
+        System.out.println("Result after activation: " + outputs[0]);
+
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -3,5 +3,8 @@ public class Main {
     public static void main(String[] args)
     {
        MLP nn = new MLP(2,2,1);
+
+       double[] input = {0,1};
+       nn.forwardPass(input);
     }
 }
