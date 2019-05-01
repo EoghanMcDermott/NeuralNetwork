66d908290e983783d948a22dbea0029ddcc2a760
Eoghan <eoghan.mc-dermott.1@ucdconnect.ie>
2019-05-01 14:00:54 +0100

    Tidied code & added comments for clarity

--- src/MLP.java
+++ src/MLP.java
@@ -6,16 +6,20 @@ public class MLP
     private int numInputs;
     private int numHiddenUnits;
     private int numOutputs;
+    //used to store network architecture parameters
 
     private double[][] w1, w2;
     private double[][] dW1, dW2;
+    //store weights and weight changes for input layer and hidden layer respectively
 
     private double[] z1, z2;
+    //store activations (weight * input before applying sigmoid) to hidden units and output respectively
 
-    private double[] inputValues;
+    private double[] inputValues;//need input values when computing deltas for lower layer
+
     private double[] hidden;
     private double[] outputs;
-
+    //store hidden and output values after sigmoid activation function is applied to the respective input
 
 
     public MLP(int ni, int nh, int no)
@@ -24,6 +28,7 @@ public class MLP
         numInputs = ni;
         numHiddenUnits = nh;
         numOutputs = no;
+        //keeping track of input parameters for network architecture
 
         w1 = new double [numInputs][numHiddenUnits];
         w2 = new double [numHiddenUnits][numOutputs];
@@ -36,7 +41,7 @@ public class MLP
 
         hidden = new double[numHiddenUnits];
         outputs = new double[numOutputs];
-        //initialise various arrays
+        //create various arrays of appropriate size
 
         w1 = randomise(w1);
         w2 = randomise(w2);
@@ -48,9 +53,11 @@ public class MLP
 
         dW1 = fill2Dzeroes(dW1);
         dW2 = fill2Dzeroes(dW2);
+        //fill weight change arrays with 0's intially so can use += later
     }
 
-    private double[][] randomise(double[][] input)//method to fill 2d array with small random values
+    private double[][] randomise(double[][] input)
+    //method to fill 2d array with small random values
     {
         Random rand = new Random();
 
@@ -59,37 +66,37 @@ public class MLP
         for(int i=0;i<input.length;i++)
         {
             for(int j=0;j<input[0].length;j++)
-                result[i][j] = rand.nextDouble();//fill array with random doubles in range [0,0.5)
+                result[i][j] = rand.nextDouble();//fill array with random doubles in range [0,1)
         }
 
         return result;
     }
 
-    private double[][] fill2Dzeroes(double[][] input)
+    private double[][] fill2Dzeroes(double[][] input)//method to fill 2D array with 0's - need for dW1 & dW2 reset in update weights method
     {
         double[][] result = new double[input.length][input[0].length];
 
         for(double[] row : result)
             Arrays.fill(row,0);
 
-        return result;
+        return result;//array of same dimension as input and filled with 0's
     }
 
     public void forwardPass(double[] input)//pass an input vector and calculate and store result in outputs array
     {
-        inputValues = input;//keep track of input data - need to calculate deltas
-        calculateHidden(input);
+        inputValues = input;//keep track of input data - need this to calculate deltas
+        calculateHidden();
         calculateOutput();
     }
 
-    private void calculateHidden(double[] input)//calculate hidden values for forward pass
+    private void calculateHidden()//calculate hidden values for forward pass
     {
-        //using matrix multiplication to calculate values in hidden units
-        for(int i=0;i<numHiddenUnits;i++)
+
+        for(int i=0;i<numHiddenUnits;i++)//using matrix multiplication to calculate values in hidden units (input * w1)
         {
             for(int j=0;j<numInputs;j++)
             {
-                z1[i] = (input[j] * w1[j][i]);//activation for hidden units
+                z1[i] = (inputValues[j] * w1[j][i]);//activation for hidden units
                 hidden[i] = sigmoid(z1[i]);//apply activation to get hidden values
             }
         }
@@ -97,7 +104,7 @@ public class MLP
 
     private void calculateOutput()//calculate output of a forward pass
     {
-        for(int i=0;i<numOutputs;i++)
+        for(int i=0;i<numOutputs;i++)//multiply input to layer (hidden values) by weights for hidden layer (hidden * w2)
         {
             for(int j=0;j<numInputs;j++)
             {
@@ -107,64 +114,65 @@ public class MLP
         }
     }
 
-    public double backProp(double[] target)
+    public double backProp(double[] target)//back propagate error and calculate weight changes - apply chain rule
     {
-              //need to compute deltas for upper layer
-        double deltaOutput = 0;
+        double deltaOutput = 0;//need to initialise a value (but calculate it in below for loop)
 
-        for(int i=0;i<numHiddenUnits;i++)
+        for(int i=0;i<numHiddenUnits;i++)//need to compute deltas for upper layer
         {
             for(int j=0;j<numOutputs;j++)
             {
                 deltaOutput = calculateError(target[j], outputs[j]) * sigmoidDerivative(z2[j]);
-                dW2[i][j] += deltaOutput * hidden[j];
+                dW2[i][j] += deltaOutput * hidden[j]; //multiply deltas by hidden values to get dW2 values
             }
-            //multiply deltas by hidden values
-            //this gives dW2 values
         }
 
-        for(int i=0;i<numInputs;i++)
+        for(int i=0;i<numInputs;i++)//compute deltas for lower layer
         {
             for(int j=0;j<numHiddenUnits;j++)
             {
                 double hiddenError = deltaOutput * sigmoidDerivative(hidden[j]);
                 double deltaHidden = hiddenError * w2[j][0];
-                dW1[i][j] +=  deltaHidden * inputValues[i];
+                dW1[i][j] +=  deltaHidden * inputValues[i]; //multiply deltas by input values to get dW1 values
             }
         }
-        //compute deltas for lower layer
-        //multiply deltas by input values
 
-        return calculateTotalError(target);
+        return calculateTotalError(target);//return total error for a given example input
     }
 
     private double calculateTotalError(double[] target)
+    //calculates total error over multiple target values (XOR and sin questions only have 1)
     {
         double totalError = 0;
 
         for(int i=0;i<numOutputs;i++)
-            totalError += calculateError(target[i], outputs[i]);
+            totalError += calculateError(target[i], outputs[i]);//summation of error
 
         return totalError;
     }
 
-    public void updateWeights(double learningRate)
+    private double calculateError(double target, double actual)//calculate error from target value
+    {
+        return  0.5*(Math.pow((target-actual),2));//mean squared error
+    }
+
+    public void updateWeights(double learningRate)//apply weight updates with learning rate
     {
         for(int i=0;i<numInputs;i++)
         {
             for(int j=0;j<numHiddenUnits;j++)
-                w1[i][j] -=dW1[i][j]*learningRate;
+                w1[i][j] -=dW1[i][j]*learningRate;//- value for learning rate
         }
 
         for(int i=0;i<numHiddenUnits;i++)
         {
             for(int j=0;j<numOutputs;j++)
-                w2[i][j] -= dW2[i][j]*learningRate;
+                w2[i][j] -= dW2[i][j]*learningRate;//- value for learning rate
         }
 
         dW1 = fill2Dzeroes(dW1);
         dW2 = fill2Dzeroes(dW2);
-        //reinitialise the arrays to set all the values to 0
+        //reset the weight change arrays to 0
     }
 
     private double sigmoid(double x)//activation function
@@ -176,14 +184,6 @@ public class MLP
     {
         return sigmoid(x)*(1.0-sigmoid(x));
     }
-
-
-
-    private double calculateError(double target, double actual)//calculate error from target value
-    {
-        return  0.5*(Math.pow((target-actual),2));//mean squared error
-    }
-
 
     public double[] getOutput()//get output value(s)
     {
