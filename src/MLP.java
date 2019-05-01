fa78d294874ae2a31732b522a1851ff6cee4c1e6
Eoghan <eoghan.mc-dermott.1@ucdconnect.ie>
2019-05-01 13:23:51 +0100

    Not working but tidied code up

--- src/MLP.java
+++ src/MLP.java
@@ -45,6 +45,24 @@ public class MLP
         Arrays.fill(hidden,0);
         Arrays.fill(outputs, 0);
         //fill weight arrays with small random initial weights
+
+        dW1 = fill2Dzeroes(dW1);
+        dW2 = fill2Dzeroes(dW2);
+    }
+
+    private double[][] randomise(double[][] input)//method to fill 2d array with small random values
+    {
+        Random rand = new Random();
+
+        double[][] result = new double[input.length][input[0].length];//output array same dimension as input array
+
+        for(int i=0;i<input.length;i++)
+        {
+            for(int j=0;j<input[0].length;j++)
+                result[i][j] = rand.nextDouble();//fill array with random doubles in range [0,0.5)
+        }
+
+        return result;
     }
 
     private double[][] fill2Dzeroes(double[][] input)
@@ -57,53 +75,36 @@ public class MLP
         return result;
     }
 
-    private double[][] randomise(double[][] input)//method to fill 2d array with small random values
+    public void forwardPass(double[] input)//pass an input vector and calculate and store result in outputs array
     {
-        Random rand = new Random();
-
-        double[][] result = new double[input.length][input[0].length];
-
-        for(int i=0;i<input.length;i++)
-        {
-            for(int j=0;j<input[0].length;j++)
-                result[i][j] = rand.nextDouble();
-        }
-
-        return result;
-    }
-
-    public void forwardPass(double[] input)
-    {
-        inputValues = input;
+        inputValues = input;//keep track of input data - need to calculate deltas
         calculateHidden(input);
         calculateOutput();
     }
 
-    private void calculateHidden(double[] input)
+    private void calculateHidden(double[] input)//calculate hidden values for forward pass
     {
         //using matrix multiplication to calculate values in hidden units
         for(int i=0;i<numHiddenUnits;i++)
         {
             for(int j=0;j<numInputs;j++)
             {
-                z1[i] = (input[j] * w1[j][i]);
-                hidden[i] = sigmoid(z1[i]);
+                z1[i] = (input[j] * w1[j][i]);//activation for hidden units
+                hidden[i] = sigmoid(z1[i]);//apply activation to get hidden values
             }
         }
     }
 
-    private void calculateOutput()
+    private void calculateOutput()//calculate output of a forward pass
     {
         for(int i=0;i<numOutputs;i++)
         {
             for(int j=0;j<numInputs;j++)
             {
-                z2[i] =z1[j] * w2[j][i];
-                outputs[i] = sigmoid(z2[i]);
+                z2[i] =z1[j] * w2[j][i];//activation for output
+                outputs[i] = sigmoid(z2[i]);//apply activation to get output values
             }
         }
-
-        printOutput();
     }
 
     public double backProp(double target)
@@ -154,8 +155,9 @@ public class MLP
                 w2[i][j] -= dW2[i][j]*learningRate;
         }
 
-       dW1 = fill2Dzeroes(dW1);
-       dW2 = fill2Dzeroes(dW2);
+        dW1 = fill2Dzeroes(dW1);
+        dW2 = fill2Dzeroes(dW2);
+        //reinitialise the arrays to set all the values to 0
     }
 
     private double sigmoid(double x)//activation function
@@ -170,51 +172,19 @@ public class MLP
 
 
 
-    private double calculateError(double target)
+    private double calculateError(double target)//calculate error from target value
     {
         double error = 0;
 
         for(double y : outputs)
-//            error += Math.abs(y-target);
             error+= 0.5*(Math.pow((target-y),2));
 
-        return error;
+        return error;//mean squared error
     }
 
-    private void printHidden()
-    {
-        for(double h : hidden)
-            System.out.println("Hidden: " + h);
-    }
 
-    private void printOutput()
+    public double[] getOutput()//get output value(s)
     {
-        for(double o : outputs)
-            System.out.println("Output: " + o);
-    }
-
-    private void printZ1()
-    {
-        for(double z : z1)
-            System.out.println("Z1: " + z);
-    }
-
-    private void printZ2()
-    {
-        for(double z : z2)
-            System.out.println("Output after activation: " + z);
-    }
-
-    private void printWeights()
-    {
-        System.out.println("w1:");
-        for(double[] row : w1)
-            for(double w : row)
-                System.out.println(w);
-
-        System.out.println("w2:");
-        for(double[] column : w2)
-            for(double w : column)
-                System.out.println(w);
+        return outputs;
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,32 +1,45 @@
+import java.io.FileWriter;
 import java.util.ArrayList;
 
 public class Main {
 
     public static void main(String[] args)
     {
-       MLP nn = new MLP(2,2,1);
-
-       double[][] input =  {{0,0}, {0,1}, {1,0}, {1,1}};
-       double[] output = {0, 1, 1, 0};
-
-       int maxEpochs = 10000;
-       double learningRate = 0.1;
-
-        for (int e=0; e<maxEpochs; e++)
+        try
         {
-            double totalError = 0;
+            FileWriter fw = new FileWriter("output.txt");
 
-            for (int i=0;i<input.length;i++)
+            MLP nn = new MLP(2,2,1);
+
+            double[][] input =  {{0,0}, {0,1}, {1,0}, {1,1}};
+            double[] output = {0, 1, 1, 0};
+
+            int maxEpochs = 300;
+            double learningRate = 0.1;
+
+            for (int e=0; e<maxEpochs; e++)
             {
-                nn.forwardPass(input[i]);
-                System.out.println("Expected: " + output[i]);
-                double thisError = nn.backProp(output[i]);
-                totalError += thisError;
+                double totalError = 0;
+
+                for (int i=0;i<input.length;i++)
+                {
+                    nn.forwardPass(input[i]);
+                    double thisError = nn.backProp(output[i]);
+                    totalError += thisError;
+                    fw.write("Expected: " + output[i] + " Predicted: " + nn.getOutput()[0]+ "\n");
+                }
+
+                nn.updateWeights(learningRate);
+
+                fw.write("Total error at epoch " + e + ": " + totalError + "\n\n");
+
             }
 
-            nn.updateWeights(learningRate);
-
-            System.out.println("Total error at epoch " + e + ": " + totalError + "\n");
+            fw.close();
+        }
+        catch (Exception e)
+        {
+            e.printStackTrace();
         }
 
 //        MLP n2 = new MLP(4, 5, 1);
